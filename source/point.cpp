1a513f9b1112d6509afbc5350b59056acea4a165
Francesco Cerroni <francescocerroni03@gmail.com>
2023-06-25 22:16:09 +0200

    implementati operatori per Point

--- source/point.cpp
+++ source/point.cpp
@@ -2,6 +2,8 @@
 
 #include <cmath>
 
+Point::Point(ftype val) { pt.fill(val); }
+
 Point::Point(ftype x, ftype y, ftype z) {
   pt[0] = x;
   pt[1] = y;
@@ -14,6 +16,10 @@ ftype& Point::operator()(int i) { return pt[i]; }
 
 ftype Point::norm() {
   return sqrt(pt[0] * pt[0] + pt[1] * pt[1] + pt[2] * pt[2]);
+}
+
+Point Point::unitVector() {
+  // TODO
 }
 
 Point Point::toSpherical() {
@@ -29,4 +35,66 @@ Point Point::toCartesian() {
   ct(1) = pt[0] * sin(this->pt[1]) * sin(pt[2]);
   ct(2) = pt[0] * cos(this->pt[1]);
   return ct;
+}
+
+Point& Point::operator+=(const Point& P) {
+  for (int i = 0; i < 3; i++) {
+    pt[i] += P(i);
+  }
+  return *this;
+}
+
+Point& Point::operator-=(const Point& P) {
+  for (int i = 0; i < 3; i++) {
+    pt[i] -= P(i);
+  }
+  return *this;
+}
+
+Point& Point::operator*=(const ftype lambda) {
+  for (int i = 0; i < 3; i++) {
+    pt[i] *= lambda;
+  }
+  return *this;
+}
+
+Point& Point::operator/=(const ftype lambda) {
+  for (int i = 0; i < 3; i++) {
+    pt[i] /= lambda;
+  }
+  return *this;
+}
+
+Point operator+(const Point& P, const Point& Q) {
+  Point R{P};
+  R += Q;
+  return R;
+}
+
+Point operator-(const Point& P, const Point& Q) {
+  Point R{P};
+  R -= Q;
+  return R;
+}
+
+Point operator*(const Point& P, const ftype lambda) {
+  Point R{P};
+  R *= lambda;
+  return R;
+}
+
+Point operator/(const Point& P, const ftype lambda) {
+  Point R{P};
+  R *= lambda;
+  return R;
+}
+
+Point operator*(const Matrix& M, const Point& P) {
+  Point Q(0.);
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      Q(i) += M(i, j) * P(j);
+    }
+  }
+  return Q;
 }
--- source/point.hpp
+++ source/point.hpp
@@ -11,15 +11,28 @@ class Point {
 
  public:
   Point() = default;
+  Point(ftype);
   Point(ftype, ftype, ftype);
 
   ftype const& operator()(int) const;
   ftype& operator()(int);
 
   ftype norm();
+  Point unitVector();
 
   Point toSpherical();  // from cartesian to spherical coordinates
   Point toCartesian();  // from spherical to cartesian coordinates
+
+  Point& operator+=(const Point&);
+  Point& operator-=(const Point&);
+  Point& operator*=(const ftype);
+  Point& operator/=(const ftype);
 };
+
+Point operator+(const Point&, const Point&);
+Point operator-(const Point&, const Point&);
+Point operator*(const Point&, const ftype);
+Point operator/(const Point&, const ftype);
+Point operator*(const Matrix&, const Point&);
 
 #endif
