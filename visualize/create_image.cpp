4cfa37d62f0c01b861ae414b36b6f7be1b39f530
Francesco Cerroni <francescocerroni03@gmail.com>
2023-06-28 11:36:57 +0200

    external problem to visualize images

---
+++ visualize/create_image.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+int main() {
+  int pixel_x;
+  int pixel_y;
+  std::vector<sf::Uint8> pixels;
+
+  std::ifstream inputFile("data.txt");
+  inputFile >> pixel_x >> pixel_y;
+  for (int i = 0; i < pixel_x * pixel_y; i++) {
+    int r, g, b;
+    inputFile >> r >> g >> b;
+    pixels.push_back(r);
+    pixels.push_back(g);
+    pixels.push_back(b);
+    pixels.push_back(255);
+  }
+  inputFile.close();
+
+  sf::Image img;
+  img.create(pixel_x, pixel_y, pixels.data());
+  img.saveToFile("image.png");
+}
