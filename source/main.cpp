63be5375f4ae3a2231efbffef5a849381377458e
Francesco Cerroni <francescocerroni03@gmail.com>
2023-06-28 15:35:54 +0200

    fixed compile error

--- source/main.cpp
+++ source/main.cpp
@@ -8,12 +8,16 @@
 
 int main() {
   Camera cam(Point(-25., -25., 0.), M_PI_4, M_PI_4, 0., 1920, 1080, 120. / 360. * M_PI * 2);
-  donut don(20., 5., 1000, 1000, RGB(255, 160, 122));
+  donut don(20., 5., 100, 100, RGB(139,0,139));
+  donut don2(20., 5., 100, 100, RGB(200, 0, 0));
   don.set_cdm(Point(0., 30., 50.));
+  don2.set_cdm(Point(0, 20, 60));
+  don2.rotate(Point(M_PI_2, 0., 0.));
   //don.rotate(Point(1., 0.3, -0.8));
 
-  auto res = render(cam, std::vector<donut>(1, don), RGB(0, 0, 0));
-  projectPoint(cam, Point(50., 50., 50.));
+  std::vector<donut> sprites = {don, don2};
+
+  auto res = render(cam, sprites, RGB(128, 128, 128));
 
   std::ofstream outFile("../visualize/data.txt");
   outFile << cam.getPixelX() << " " << cam.getPixelY() << "\n";
