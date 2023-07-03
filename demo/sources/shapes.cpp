2ceb4ddfea0b4ba9311bb2e33133957d16c6f342
Francesco Cerroni <francescocerroni03@gmail.com>
2023-07-03 12:13:31 +0200

    updated shapes demo

--- demo/sources/shapes.cpp
+++ demo/sources/shapes.cpp
@@ -7,11 +7,18 @@ int main() {
   Camera cam(Point(-25., -25., 0.), M_PI_4, M_PI_4, 0., 1920, 1080,
              120. / 360. * M_PI * 2);
 
-  Cylinder cil1(15., 10., 12., 10000, RGB(200, 0, 0));
-  cil1.setCenter(Point(0., 30., 50.));
-  cil1.rotate(Point(0, M_PI_2, M_PI_2));
-  std::vector<Shape*> sprites{};
-  sprites.push_back(&cil1);
+  Donut don(6., 2., 100000, RGB(0, 200, 0));
+  don.setCenter(Point(5., -5., 20.));
+  don.rotate(Point(0., 1., 0.));
+
+  Ellipsoid ell(14., 8., 5., 100000, RGB(0, 0, 200));
+  ell.setCenter(Point(0., 0., 60.));
+
+  Cylinder cil(15., 10., 12., 10000, RGB(200, 0, 0));
+  cil.setCenter(Point(0., 30., 50.));
+  cil.rotate(Point(0, M_PI_2, M_PI_2));
+  
+  std::vector<Shape*> sprites = {&don, &ell, &cil};
   auto res = render(cam, sprites, RGB(128, 128, 128));
 
   std::vector<sf::Uint8> pixels;
